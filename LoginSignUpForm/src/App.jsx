15edcdfc3042628c227f243e288f63d813cdf831
Sidheswar-lab <mahapatrasidheswar610@gmail.com>
2025-07-12 20:42:12 +0530

    Creating LogIn component

--- LoginSignUpForm/src/App.jsx
+++ LoginSignUpForm/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Login } from './Components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Login />
     </>
   )
 }
