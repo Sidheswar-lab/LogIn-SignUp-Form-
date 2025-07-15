1a23add3a6dfc99c07e49ffca1a60169c864ed5d
Sidheswar-lab <mahapatrasidheswar610@gmail.com>
2025-07-15 21:42:06 +0530

    Adding the Google Auth to the Signup page

--- LoginSignUpForm/src/main.jsx
+++ LoginSignUpForm/src/main.jsx
@@ -2,22 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter,RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import { Login } from './Components/Login.jsx'
-import {Signup} from './Components/Signup.jsx'
+import { Signup } from './Components/Signup.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
 
 const router = createBrowserRouter([
   {
     path: '/',
-    children:[
-      {index: true, Component:Signup},
-      {path: 'login', Component:Login}
+    children: [
+      { index: true, Component: Signup },
+      { path: 'login', Component: Login }
     ]
   }
 ])
+const CLIENT_ID = '699334000039-5cm1fonajgmgd69vdj9esq0jg0o9alcn.apps.googleusercontent.com'
+
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>,
+  <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </GoogleOAuthProvider>
 )
